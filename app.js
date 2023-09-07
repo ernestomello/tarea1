d50c80d5ef4964ad608c2072d878d30bc197ede1
Ernesto Mello <emello969@gmail.com>
2023-09-06 22:14:58 -0300

    pruebas con los token y las api de auth

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var app = express();
-var db = require('./db');
+//var db = require('./db');
+// Conexion a mongo
+var mongoose = require('mongoose');
+var dev_db_url = 'mongodb+srv://ernesto:SmallWork1541@cluster0.yol4sxm.mongodb.net/?retryWrites=true&w=majority';
+var mongoDB = dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+console.log('conectado Mongo');
 global.__root   = __dirname + '/'; 
 
 
@@ -17,5 +26,12 @@ app.use('/api/auth', AuthController);*/
 
 var AuthController = require('./auth/AuthController');
 app.use('/api/auth', AuthController);
+
+
+var port =  3500;
+
+var server = app.listen(port, function() {
+  console.log('Express server listening on port ' + port);
+});
 
 module.exports = app;
--- auth/AuthController.js
+++ auth/AuthController.js
@@ -18,20 +18,26 @@ router.post('/register', function(req, res) {
         name : req.body.name,
         email : req.body.email,
         password : hashedPassword
-    },
-    function (err, user) {
-        if (err) return res.status(500).send("Error")
-        // crear token
-        var token = jwt.sign({ id: user._id }, config.secret, {
-        expiresIn: 86400 // expira en 24 hours
-        });
-        res.status(200).send({ auth: true, token: token });
-    });
+    })   
+        .catch((err)=>{
+			if (err) 
+				return res.status(500).send("Error")
+        	
+		})
+        .then((user)=>{
+			// crear token
+        	var token = jwt.sign({ id: user._id }, config.secret, {
+			expiresIn: 86400 // expira en 24 hours
+			});
+			res.status(200).send({ auth: true, token: token })
+		});
+		
 });
 
 
 router.get('/me', function(req, res) {
 	var token = req.headers['x-access-token'];
+	console.log('toquen'+token);
 	if (!token) return res.status(401).send({ auth: false, message: 'Sin token' });
 	jwt.verify(token, config.secret, function(err, decoded) {
 		if (err) return res.status(500).send({ auth: false, message: 'Error de autenticacion' });
@@ -55,10 +61,16 @@ router.get('/me2', VerifyToken, function(req, res, next) {
 			res.status(200).send(user);
 		});
 });
-router.post('/login', function(req, res) {
-	User.findOne({ email: req.body.email }, function (err, user) {
+router.post('/login', async function(req, res) {
+	
+	await User.findOne({ email: req.body.email })
+	.catch((err, user) => {
 		if (err) return res.status(500).send('Error.');
 		if (!user) return res.status(404).send('No existe usuario.');
+	})
+	.then((user)=> {
+		console.log(user);
+		console.log(req.body.password);
 		var passwordIsValid = bcrypt.compareSync(req.body.password,user.password);
 		if (!passwordIsValid) return res.status(401).send({ auth: false,token: null });
 		var token = jwt.sign({ id: user._id }, config.secret, {
